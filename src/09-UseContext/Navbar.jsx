ed232082e6ba5b50844e941bc80e8e7b3da2765a
xxavierargentino <xxavierargentino@hotmail.com>
2022-11-21 13:48:19 -0300

    NavLink primer uso

--- src/09-UseContext/Navbar.jsx
+++ src/09-UseContext/Navbar.jsx
@@ -1,23 +1,35 @@
-import { Link } from "react-router-dom"
+import { Link, NavLink } from "react-router-dom"
 
 
 
 export const Navbar = () => {
   return (
     <>
-      <nav className="navbar navbar-expand-lg navbar-light bg-primary">
+      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
   <div className="container-fluid">
     <Link to='/' className="navbar-brand" >Navbar</Link>    
     <div className="collapse navbar-collapse" id="navbarNav">
       <ul className="navbar-nav">
         <li className="nav-item">
-          <Link to='/' className="nav-link active" aria-current="page" >Home</Link>
+          <NavLink 
+          className={ ({isActive})=> `nav-link ${isActive ? 'active': ''}` }
+          to='/'>
+          Home
+          </NavLink>
         </li>
         <li className="nav-item">
-          <Link to='/login' className="nav-link" >Login</Link>
+        <NavLink 
+          className={ ({isActive})=> `nav-link ${isActive ? 'active': ''}` }
+          to='/login'>
+          Login
+          </NavLink>
         </li>
         <li className="nav-item">
-          <Link to='/about' className="nav-link" >About</Link>
+        <NavLink 
+          className={ ({isActive})=> `nav-link ${isActive ? 'active': ''}` }
+          to='/about'>
+          About
+          </NavLink>
         </li>
       
       </ul>
